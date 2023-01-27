b1d1bf09a555bfe5eb80171849feb6acc8784186
dango <a15521474798@outlook.com>
2023-01-27 19:34:28 +0800

    配置过滤器

--- src/main/java/com/dango/reggie/ReggieTakeoutApplication.java
+++ src/main/java/com/dango/reggie/ReggieTakeoutApplication.java
@@ -3,8 +3,11 @@ package com.dango.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieTakeoutApplication {
 
 	public static void main(String[] args) {
