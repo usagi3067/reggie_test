7085a3fadb145b90d62f6c4d65436cf643623c29
dango <a15521474798@outlook.com>
2023-01-29 00:22:02 +0800

    新增分类实现

--- src/main/java/com/dango/reggie/controller/CategoryController.java
+++ src/main/java/com/dango/reggie/controller/CategoryController.java
@@ -1,13 +1,28 @@
 package com.dango.reggie.controller;
 
+import com.dango.reggie.common.R;
+import com.dango.reggie.entity.Category;
 import com.dango.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/category")
+@Slf4j
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        log.info("category:{}", category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
 }
