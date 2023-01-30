bb5f81947bba049df4935927d505580a1d0ef0e1
dango <a15521474798@outlook.com>
2023-01-30 22:41:19 +0800

    菜品分页查询功能实现（不完善）

--- src/main/java/com/dango/reggie/controller/DishController.java
+++ src/main/java/com/dango/reggie/controller/DishController.java
@@ -1,5 +1,7 @@
 package com.dango.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dango.reggie.common.R;
 import com.dango.reggie.dto.DishDto;
 import com.dango.reggie.entity.Dish;
@@ -9,10 +11,7 @@ import com.dango.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 菜品管理
@@ -33,5 +32,31 @@ public class DishController {
 
         dishService.saveWithFlavor(dishDto);
         return R.success("新增菜品成功");
+    }
+
+    /**
+     * 菜品信息分页查询
+     * @param page
+     * @param pageSize
+     * @param name
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page, int pageSize, String name) {
+        //构造分页构造器对象
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+
+        //构造条件构造器
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.like(name != null, Dish::getName, name);
+
+        //添加排序条件
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+        //执行分页查询
+        dishService.page(pageInfo, queryWrapper);
+
+        return R.success(pageInfo);
     }
 }
